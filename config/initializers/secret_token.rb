85e76134a766caf352b522b3f4603911af849a9b
ThesoroMac <rob.whitehead@gmail.com>
2013-02-12 12:55:52 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Naknower::Application.config.secret_token = '32f313999d8d85f652352a827bf4c7e41dbebcbd9b06a89ee02ffcd351a244e8987a56e0961cbf2ad22a4ec22fc310bd735d072043e06f8cc2df8ed59ca63cf1'
